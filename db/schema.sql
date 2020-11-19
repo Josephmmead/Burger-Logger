17408a7d46987aa166cf49883a4f125b717a86cb
Joseph Mead <josephmmead@gmail.com>
2020-11-19 15:40:54 -0500

    set up connection and orm files

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,12 @@
 ###Schema
 
-
-DROP DATABASE burgers_db;
-
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR,
-    devoured BOOLEAN
+    burger_name VARCHAR (255),
+    devoured BOOLEAN,
     PRIMARY KEY (id)
 );
