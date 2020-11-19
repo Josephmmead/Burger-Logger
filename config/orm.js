667c4ddc1dcb0ed027ce59c84e96a927b9a127e2
Joseph Mead <josephmmead@gmail.com>
2020-11-19 16:14:50 -0500

    added models, controllers and started on handlebars

--- config/orm.js
+++ config/orm.js
@@ -25,12 +25,10 @@ function objToSql(ob) {
     var value = ob[key];
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
@@ -70,7 +68,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+  
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -90,5 +88,5 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
+ 
 module.exports = orm;
---
+++ controllers/burgerController.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const burger = require("../models/burger");
+
+router.get("/", function(req, res){
+    burger.all(function(results){
+        res.render("index", {burgers: results} )
+    })
+});
+
+
+router.post("/burgers/create", function(req, res){
+    burger.create(req.body.burger_name, function(results){
+        console.log(results);
+        res.redirect("/")
+    })
+})
+
+router.put("/burgers/:id", function(req, res){
+    burger.update(req.params.id, function(results){
+        console.log(results);
+        res.sendStatus(200)
+    })
+})
+
+module.exports = router;
